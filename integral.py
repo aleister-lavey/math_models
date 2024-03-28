1aad57474ecec0f43fac4638f853e3a56161cb71
Pavel <bratash.pavel@mail.ru>
2024-03-28 17:28:20 +0300

    improved integral methods

--- integral.py
+++ integral.py
@@ -2,29 +2,39 @@ import numpy as np
 
 #first example function
 def f(x):
-    return (x ** 5 + np.sin(x) + np.tan(x / 2)) / (np.sin(37 * x))
+    return np.sin(x)
 
 # interval [a,b]
-a = 2
-b = 4
-S1 = 0
+a = 0
+b = np.pi
 
 N = int(input("input approximity number: "))
-
 
 #rectangle method
 rectangle_width = (b - a) / N
 
+#right rectangle area method
 
+area_right = 0
 
 for i in range(N):
-    S1 += rectangle_width * f(a + rectangle_width * i)
-print(f'rectangle method area = {S1}')
+    area_right += rectangle_width * f(a + rectangle_width * i)
+print(f'rigth rectangle method area = {area_right}')
 
-S2 = 0
+#left rectangle area method
+
+area_left = 0
+
+for i in range(1,N):
+    area_left += rectangle_width * f(a + rectangle_width * i - rectangle_width)
+print(f'left rectangle method area = {area_left}')
+
+area_trap = 0
+
 #trapezoid method
+
 j = 1
 for i in range(N):
-    S2 += rectangle_width * ( (f(a + i * rectangle_width) + f(a + j * rectangle_width)) / 2)
+    area_trap += rectangle_width * ( (f(a + i * rectangle_width) + f(a + j * rectangle_width)) / 2)
     j += 1
-print(f'trapezoid method area = {S2}')
+print(f'trapezoid method area = {area_trap}')
