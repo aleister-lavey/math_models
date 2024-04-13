803a4130744e3c0ac43b9e7b50febabf97e27397
aleister <aleister@mail.ru>
2024-04-14 00:43:18 +0300

    fixed grad method

--- grad_descent.py
+++ grad_descent.py
@@ -2,42 +2,36 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 def f(x):
     return np.cos(5 * x) * x ** 3
 
-
 def df(x):
-    return x ** 2 * (1.5 * np.cos(5 * x) - 2.5 * x * np.sin(5 * x))
+    return 3 * x ** 2 * np.cos(5 * x) - 5 * x ** 3 * np.sin(5 * x) 
 
-
-#learning_rate = 0.1
-grad_iterations = 100
-step_size = 100
-
-print(".............GRADIENT......DESCENT.....................")
-#x_min = float(input("Input minimal x:"))
-#x_max = float(input("Input max x:"))
-x = 0
-x_max = 3
+y_min_global = 0
 x_min = -3
-y_min_global = f(x)
-for _ in range(100):
-    x = random.uniform(-3,3)
-    
-    for i in range(grad_iterations):
-        learning_rate = 1 / min( i + 1, step_size)
-        #learning_rate = 0.01
+x_max = 3
+grad_iterations = 100
+grad_cycles = 1000
+grad_step = 100
+
+for _ in range(grad_iterations):
+    x = random.uniform(x_min,x_max)
+
+    for i in range(grad_cycles):
+        learning_rate = 1 / min( i + 10, grad_step)
         x = x - learning_rate * np.sign(df(x))
-    y_min_local = f(x)
-if y_min_global > y_min_local:
-     y_min_global = y_min_local
-    
-print(f"Minimal function value on segment ({x_min};{x_max}):","x=", round(x,3),"y=", round(y_min_global,3))
+        if x < x_min - (x_min / 10)  or x > x_max - (x_max / 10):
+            break
+        else:
+            y_min_local = f(x)
+        if y_min_local < y_min_global:
+            y_min_global = y_min_local
+
+print(f'global minimum is: {y_min_global}')
 
 x_plt = np.arange(x_min, x_max, 0.1)
 f_plt = [f(x) for x in x_plt]
-
 plt.ion()
 fig, ax = plt.subplots()
 ax.grid(True)
