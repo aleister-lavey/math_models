0ecb99987c59f0e4daea5a49f214430b85b3e558
aleister-lavey <45740999+aleister-lavey@users.noreply.github.com>
2024-05-16 11:19:26 +0300

    Update equation.py

--- equation.py
+++ equation.py
@@ -9,12 +9,19 @@ a = -2
 b = -1
 c = (a + b) / 2
 
-#left_interval = np.arange(a , c , 0.001)
-#right_interval = np.arange(c , b , 0.001)
-
-#for i in left_interval:
-#    if np.sign(f(i)) != np.sign(f(i - 0.001)):
-#        print('found root')
+#x_left = a
+#x_right = b
+#eps = 0.0001
+#
+#while (x_right - x_left > eps):
+#   cent = (x_right - x_left) / 2
+#    #print(abs(x_right) - abs(x_left))
+#    x_i = x_left + cent
+#    if np.sign(f(x_left)) != np.sign(f(x_right)):
+#        x_right = x_i
+#    else:
+#        x_left = x_i
+#print(x_i)
 
 x_plt = np.arange(a, b, 0.001)
 f_plt = [f(x) for x in x_plt]
